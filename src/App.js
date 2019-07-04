7fa5b474e65e870a2ef9c6c8070945f39c613746
julianeon <julianeon@gmail.com>
2019-07-03 21:32:40 -0700

    Removed unused component, added back in main text.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Typing from 'react-typing-animation';
 
-function Spree() {
-    return (
-	    <p>That is all she wrote.</p>
-    )
-}
-
-function FormSpree() {
-    return (
-	    <form action="https://formspree.io/email@domain.tld" method="POST">
-	    <input type="text" name="name"/>
-	    <input type="email" name="_replyto"/>
-	    <input type="submit" value="Send"/>
-	    </form>
-    )
-}
-
 function AniType(props) {
     var word=[]
     props.text.map(item => word.push(<React.Fragment><span>{item}</span><br/><Typing.Delay ms={1000} /><Typing.Reset count={3} delay={1000} /></React.Fragment>)); 
     
     return (
-	    <Typing><span>click here to RSVP</span></Typing>
+	    <Typing>{word}></Typing>
     );
 }
 
